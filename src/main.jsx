51fb88c7a41a06e0e9dc59f3a92a1fac64ee2088
Yair katan <yairkatan5@gmail.com>
2026-04-29 18:44:57 +0300

    Enable admin provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { AdminProvider } from './admin/AdminContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AdminProvider>
+      <App />
+    </AdminProvider>
   </React.StrictMode>
 );
